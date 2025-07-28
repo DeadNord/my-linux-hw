9643143e6cdf7ae0a1d54c1d7e7d4f354b3500b7
Yegor Osipov <83457266+DeadNord@users.noreply.github.com>
2025-07-28 22:37:09 +0200

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -18,6 +18,12 @@ output "internet_gateway_id" {
   value       = aws_internet_gateway.igw.id
 }
 
+output "vpc_cidr_block" {
+  description = "CIDR блок створеної VPC"
+  value       = aws_vpc.main.cidr_block
+}
+
+
 #output "nat_gateway_id" {
 #  description = "ID NAT-інстансу"
 #  value       = aws_nat_gateway.nat.id
